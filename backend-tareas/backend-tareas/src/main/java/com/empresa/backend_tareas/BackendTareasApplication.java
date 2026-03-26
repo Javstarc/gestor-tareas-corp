c3d483ee07d9e53bff4e7ba9fad92f143122d287
Javstarc <javricarcor@hotmail.com>
2026-03-25 22:21:14 -0500

    chore: asgraga estructura inicial de Spring Boot para el backend

---
+++ backend-tareas/backend-tareas/src/main/java/com/empresa/backend_tareas/BackendTareasApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.backend_tareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTareasApplication.class, args);
+	}
+
+}
---
+++ backend-tareas/backend-tareas/src/test/java/com/empresa/backend_tareas/BackendTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.backend_tareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
